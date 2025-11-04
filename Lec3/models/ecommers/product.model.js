d9284f72d663c65da4ec135dfb4d2797e3374db3
Rohit Paul <rohitpaul9734@gmail.com>
2025-11-04 13:14:45 +0530

    Add order model

--- Lec3/models/ecommers/product.model.js
+++ Lec3/models/ecommers/product.model.js
@@ -1,7 +1,33 @@
 import mongoose from "mongoose";
-const productSchema= mongoose.Schema({
-    
-},{timestamps:true})
-
+const productSchema = mongoose.Schema(
+  {
+    description: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+    productImage: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default:0,
+    },
+    stock: {
+      type: Number,
+      deafult:0,
+    },
+    catagory:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Catagory',
+        required:true,
+        
+    }
+  },
+  { timestamps: true }
+);
 
 export const Product = mongoose.model("Product", productSchema);
